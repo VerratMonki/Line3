5907880603dcec125a3ea195aa27518fb3ce23fb
VerratMonki <sanik.nikon@gmail.com>
2020-03-07 20:18:04 +0200

    Start tests of multi-threads

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,13 @@
 package main;
 
 import org.linereader.impl.*;
-import org.linereader.interfaces.Formatter;
 import org.linereader.interfaces.GetData;
 import org.linereader.interfaces.LineConsumer;
 import org.linereader.interfaces.OnError;
 
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
@@ -21,9 +21,17 @@ public class Main {
 			OnError onError = ex -> {
 				System.err.println(ex);
 			};
+		CountDownLatch countDownLatch = new CountDownLatch(2);
+			DemoThread firstStream = new DemoThread(countDownLatch);
+			DemoThread secondStream = new DemoThread(countDownLatch);
+			Thread firstThread = new Thread(firstStream);
+			Thread secondThread = new Thread(secondStream);
+			firstThread.start();
+			secondThread.start();
+
 		//Formatter formatter = new HTML();
 		//Formatter formatter = new TXT(onError);
-		Formatter formatter = new XML();
+		/*Formatter formatter = new XML();
 			DefaultReader defaultReader = new DefaultReader("test.txt",
 					counters,
 					onError, formatter, getData);
@@ -32,7 +40,31 @@ public class Main {
 			}catch (Exception ex)
 			{
 				onError.onError(ex);
-			}
+			}*/
+
+	}
+}
+
+class DemoThread implements Runnable
+{
+CountDownLatch countDownLatch;
+	DemoThread(CountDownLatch countDownLatch)
+	{
+		this.countDownLatch = countDownLatch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			countDownLatch.countDown();
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for(int i=0;i<100000000;i++)
+		{
+			System.out.println(i + ", ");
+		}
 
 	}
 }
